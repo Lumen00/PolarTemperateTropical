63fa16cfb88501b4d596f80f343a809be10df540
Damian Sue <damianbsue@gmail.com>
2023-12-16 15:09:57 +1100

    Updated app.py for model.pkl

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
+# Load the model downloaded from Kaggle.
+learn = load_learner('model.pkl')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+# Define a prediction function for the model.
+labels = ('Polar', 'Temperate', 'Tropical')
+
+
+def predict(img):
+    pred, pred_idx, probs = learn.predict(img)
+    return dict(zip(labels, map(float, probs)))
+
+# Define gradio inputs and outputs.
+image = gr.inputs.Image(shape=(192, 192))
+label = gr.outputs.Label()
+
+iface = gr.Interface(fn=predict, inputs=image, outputs=label)
+iface.launch(share=True)
